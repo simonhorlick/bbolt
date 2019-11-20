6283eca115d4294f55f21b6496206f1eb795735f
Simon Horlick <simonhorlick@gmail.com>
2019-11-20 19:54:21 +0800

    Initial commit

---
+++ ios/Classes/BboltPlugin.m
@@ -0,0 +1,20 @@
+#import "BboltPlugin.h"
+
+@implementation BboltPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"bbolt"
+            binaryMessenger:[registrar messenger]];
+  BboltPlugin* instance = [[BboltPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
